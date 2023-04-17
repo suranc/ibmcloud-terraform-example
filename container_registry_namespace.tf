97e1ea88566f1456c4445542a5217c96645e5038
Chris Suran <christopher.suran@harness.io>
2023-04-17 16:10:09 -0400

    Fix sleep

--- container_registry_namespace.tf
+++ container_registry_namespace.tf
@@ -1,4 +1,9 @@
 resource "ibm_cr_namespace" "cr_namespace" {
   name = "suranc-example-ns"
+  
+  depends_on = [time_sleep.wait_10_minutes]
+}
+
+resource "time_sleep" "wait_10_minutes" {
   create_duration = "10m"
 }
